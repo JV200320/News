5129bb1bdd051dc4f7f3ac9795fec8de5f8dff63
João Victor <joaovictordev2003@gmail.com>
2021-04-24 23:04:14 -0300

    Beginning

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
+
+const routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={ } />
+      </Switch>
+    </BrowserRouter>
+  )
+
+}
+export default routes;
